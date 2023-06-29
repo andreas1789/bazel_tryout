719d029c7a2a2c9e51bbd5608c51450d39bb601d
Andreas Hoffmann <andreashoffmann1789l@gmail.com>
2023-06-30 00:10:28 +0200

    add lib

--- main/file1.cpp
+++ main/file1.cpp
@@ -2,10 +2,12 @@
 #include "file1.h"
 
 extern std::string get_text();
+extern int get_number();
 
 int main()
 {
     std::cout << text << "\n";
     std::cout << get_text();
+    std::cout << get_number() << "\n";
     return 0;
 }
